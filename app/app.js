6f8d9b477b51aba70519425eeb7582702ae1ab24
Matthew Pace <pace4@purdue.edu>
2015-10-05 16:49:21 -0400

    Add some gui for selecting when you want to leave by

--- app/app.js
+++ app/app.js
@@ -1,8 +1,22 @@
 var app = angular.module('myApp', ['ngRoute', 'ngMaterial'])
 
 app.controller('AppCtrl', ['$scope', function($scope){
-  
+  $scope.yesterday = getYesterday();
+  $scope.oneYear = getOneYear();
+  $scope.pickedDate = new Date();
 }]);
+
+function getYesterday() {
+  var date = new Date();
+  date.setDate(date.getDate() - 1);
+  return date;
+}
+
+function getOneYear() {
+  var date = new Date();
+  date.setDate(date.getDate() + 365);
+  return date;
+}
 
 app.config(['$routeProvider', function($routeProvider) {
 
