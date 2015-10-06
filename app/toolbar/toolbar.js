7da2d228ee61d43c6b07e6923a8287c52e5a3819
Matthew Pace <pace4@purdue.edu>
2015-10-05 20:40:34 -0400

    Add toolbar directive and controller

---
+++ app/toolbar/toolbar.js
@@ -0,0 +1,10 @@
+app.directive('toolbar', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'toolbar/toolbar.html'
+  };
+});
+
+app.controller('ToolbarController', ['$scope', function($scope){
+  
+}]);
