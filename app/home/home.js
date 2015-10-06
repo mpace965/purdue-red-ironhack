03d035f0bf623187e34d6543473bd60539168cb8
Matthew Pace <pace4@purdue.edu>
2015-10-05 21:37:27 -0400

    Add functionality for buttons to go to trip page

--- app/home/home.js
+++ app/home/home.js
@@ -2,6 +2,15 @@ app.controller('HomeController', ['$scope', function($scope){
   $scope.yesterday = getYesterday();
   $scope.oneYear = getOneYear();
   $scope.pickedDate = new Date();
+
+  $scope.navigate = function() {
+    window.location.href = "#/trip/" + $scope.pickedDate.getFullYear() + "/" + $scope.pickedDate.getMonth() + "/" + $scope.pickedDate.getDate();
+  }
+
+  $scope.inMonth = function(months) {
+    $scope.pickedDate.setMonth($scope.pickedDate.getMonth() + months);
+    $scope.navigate();
+  }
 }]);
 
 function getYesterday() {
@@ -11,7 +20,7 @@ function getYesterday() {
 }
 
 function getOneYear() {
-  var date = new Date();
-  date.setDate(date.getDate() + 365);
+  var date = getYesterday();
+  date.setFullYear(date.getFullYear() + 1);
   return date;
 }
--- app/trip/trip.js
+++ app/trip/trip.js
@@ -1,3 +1,3 @@
 app.controller('TripController', ['$scope', '$routeParams', function($scope, $routeParams){
-  $scope.leaveDate = new Date($routeParams.year, $routeParams.month - 1, $routeParams.day);
+  $scope.leaveDate = new Date($routeParams.year, $routeParams.month, $routeParams.day);
 }]);
